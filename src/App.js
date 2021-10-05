4dcbd9310c634c4fc2ad51cb17135bf58f604e4d
shaffanFast <k190863@nu.edu.pk>
2021-10-05 15:49:23 +0500

    a

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import MathQuiz from './containers/MathQuiz';
 import './App.css';
 import backgroundGIF from './assets/gif/background.gif'
 import Footer from './components/Footer';
+import grocerySound from './assets/sounds/grocerySound.mp3'
+import on from './assets/sound.png'
+import off from './assets/mute.png'
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,11 +17,16 @@ import {
   Link
 } from "react-router-dom";
 class App extends Component {
-  
+  state = {
+    img: on,
+    // sound: false,
+    // curr: this.notPlayAudioWithVideo
+    sound: new Audio(grocerySound)
+  }
   gameStart = () => {
     this.props.onStartGame();
   }
-  handleURL(){
+  handleURL() {
     // const query = new URLSearchParams(this.props.location.search);
     // const token = query.get('id')
     // const queryParams = new URLSearchParams(window.location.search);
@@ -25,17 +34,33 @@ class App extends Component {
 
     // alert(id)
   }
+  handleSoundClick = () => {
+    if (!this.state.sound.paused) {
+      this.state.sound.pause()
+      this.setState({
+        img: off
+      })
+    }
+    else if (this.state.sound.paused) {
+      this.state.sound.play()
+      this.setState({
+        img: on
+      })
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-        <img src={backgroundGIF} id="bg" alt="" />
-
+          <img src={backgroundGIF} id="bg" alt="" />
+          <div >
+            <img alt="mute" src={this.state.img} style={{ position: "fixed", top: "20px", left: "20px", maxWidth: "40px", width: "100%", zIndex: 4 }} onClick={this.handleSoundClick} />
+          </div>
           {
             !this.props.isStarted ? (
               <Start startPressed={this.gameStart} />
             ) : (
-              <MathQuiz { ...this.props} gameStart={this.gameStart}/>
+              <MathQuiz {...this.props} gameStart={this.gameStart} />
             )
           }
         </header>
