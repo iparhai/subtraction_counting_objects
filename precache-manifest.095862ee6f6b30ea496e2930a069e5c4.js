d69960628aacbe3c833ac993c3d8c32eca2ddf63
devshaffan <devshaffan@gmail.com>
2021-11-21 00:04:12 +0500

    Updates

---
+++ precache-manifest.095862ee6f6b30ea496e2930a069e5c4.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a30e4ac4895ecfc6ab3",
+    "url": "/Subtraction_CountingObjects/static/css/main.0008348d.chunk.css"
+  },
+  {
+    "revision": "1a30e4ac4895ecfc6ab3",
+    "url": "/Subtraction_CountingObjects/static/js/main.07fa38c5.chunk.js"
+  },
+  {
+    "revision": "93c0ef82d621cfe1c7a9",
+    "url": "/Subtraction_CountingObjects/static/js/runtime~main.6d2ab770.js"
+  },
+  {
+    "revision": "dc35986270237f4445ae",
+    "url": "/Subtraction_CountingObjects/static/css/2.8eebcba9.chunk.css"
+  },
+  {
+    "revision": "dc35986270237f4445ae",
+    "url": "/Subtraction_CountingObjects/static/js/2.071cc15b.chunk.js"
+  },
+  {
+    "revision": "1ddffff5f4ae930e9f2f",
+    "url": "/Subtraction_CountingObjects/static/js/3.e97662f7.chunk.js"
+  },
+  {
+    "revision": "91412d063f2fde22edf3a015fa63a786",
+    "url": "/Subtraction_CountingObjects/static/media/removeItem.91412d06.mp3"
+  },
+  {
+    "revision": "cabc22c6e80bc043c10b098b6b8854bb",
+    "url": "/Subtraction_CountingObjects/static/media/bowl.cabc22c6.png"
+  },
+  {
+    "revision": "57a2639bd6d80374aaf0714a48200f92",
+    "url": "/Subtraction_CountingObjects/static/media/10.57a2639b.svg"
+  },
+  {
+    "revision": "a7f0e4e3c9b56642615788ea350047d6",
+    "url": "/Subtraction_CountingObjects/static/media/1.a7f0e4e3.svg"
+  },
+  {
+    "revision": "7360a269fa89ec8922a9fb9354820523",
+    "url": "/Subtraction_CountingObjects/static/media/sound.7360a269.png"
+  },
+  {
+    "revision": "2fd9ac2208c833754484473decfc84a1",
+    "url": "/Subtraction_CountingObjects/static/media/drop.2fd9ac22.wav"
+  },
+  {
+    "revision": "424d18986561c7039221eb97a66aef6b",
+    "url": "/Subtraction_CountingObjects/static/media/tostartpressbutton.424d1898.mp3"
+  },
+  {
+    "revision": "8efff185f64e4259ae68a4074dbfb0e6",
+    "url": "/Subtraction_CountingObjects/static/media/321go.8efff185.mp3"
+  },
+  {
+    "revision": "09b0b7bb4e8aad40481825ec9d91d223",
+    "url": "/Subtraction_CountingObjects/static/media/rightAnswer.09b0b7bb.mp3"
+  },
+  {
+    "revision": "ddcbc59b2917e3dbeb737fca1e3ede4e",
+    "url": "/Subtraction_CountingObjects/static/media/wrongAnswer.ddcbc59b.mp3"
+  },
+  {
+    "revision": "7890a5da3912ba0d306d379bc66c77f3",
+    "url": "/Subtraction_CountingObjects/static/media/rooster.7890a5da.png"
+  },
+  {
+    "revision": "40d8e09c1bc1e01df2f2d0350718c089",
+    "url": "/Subtraction_CountingObjects/static/media/_1.40d8e09c.mp3"
+  },
+  {
+    "revision": "65a6b97dc7801719b2d140c796276265",
+    "url": "/Subtraction_CountingObjects/static/media/_2.65a6b97d.mp3"
+  },
+  {
+    "revision": "6bd419110b7fb9b6b44fd2f477a35abc",
+    "url": "/Subtraction_CountingObjects/static/media/_3.6bd41911.mp3"
+  },
+  {
+    "revision": "ae5c58d75da67259d8f8a689eb4040cd",
+    "url": "/Subtraction_CountingObjects/static/media/_4.ae5c58d7.mp3"
+  },
+  {
+    "revision": "fa32eb88ca708bfdd1f506d0d6ad7163",
+    "url": "/Subtraction_CountingObjects/static/media/_5.fa32eb88.mp3"
+  },
+  {
+    "revision": "639f363f9c8589d7472737d7d86ca22b",
+    "url": "/Subtraction_CountingObjects/static/media/_6.639f363f.mp3"
+  },
+  {
+    "revision": "7fc0455545ca15ac611aabfd2aa9d5fc",
+    "url": "/Subtraction_CountingObjects/static/media/_7.7fc04555.mp3"
+  },
+  {
+    "revision": "9c9b9bcb08b586e22914148c5f69bcd6",
+    "url": "/Subtraction_CountingObjects/static/media/_8.9c9b9bcb.mp3"
+  },
+  {
+    "revision": "fc30493cbaf5493f1204486e9e9cbbf1",
+    "url": "/Subtraction_CountingObjects/static/media/_9.fc30493c.mp3"
+  },
+  {
+    "revision": "594c0b0ed7da5faa738c12be102b3018",
+    "url": "/Subtraction_CountingObjects/static/media/_10.594c0b0e.mp3"
+  },
+  {
+    "revision": "8164e48c115fae4bb874ed0c63b40cbc",
+    "url": "/Subtraction_CountingObjects/static/media/instructions.8164e48c.gif"
+  },
+  {
+    "revision": "612faa13f56ad11ff00e79ec305555fb",
+    "url": "/Subtraction_CountingObjects/static/media/background.612faa13.gif"
+  },
+  {
+    "revision": "d6687daa9e8311c3106547e339b95f38",
+    "url": "/Subtraction_CountingObjects/static/media/background2.d6687daa.gif"
+  },
+  {
+    "revision": "a0d6b13874a29393b0f8252f2adb5fa7",
+    "url": "/Subtraction_CountingObjects/static/media/grocerySound.a0d6b138.mp3"
+  },
+  {
+    "revision": "f02ac89a44b28acd80cb6b0399adbf8f",
+    "url": "/Subtraction_CountingObjects/static/media/mute.f02ac89a.png"
+  },
+  {
+    "revision": "2088a2c7f069d5fdf96c2aa3221d8f78",
+    "url": "/Subtraction_CountingObjects/index.html"
+  }
+];
